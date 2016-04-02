4808ecc33e40008c4d3334212c19f669de76bc15
seanpaultaylor <setaylor@blackberry.com>
2016-04-02 16:20:35 -0400

    Updated Editor window icons

--- tools/editor/src/EditorWindow.cpp
+++ tools/editor/src/EditorWindow.cpp
@@ -88,12 +88,22 @@ EditorWindow::EditorWindow(QWidget* parent) :
     QToolButton* addButton = (QToolButton*)toolBar->widgetForAction(_ui->actionAdd);
     addButton->setPopupMode(QToolButton::InstantPopup);
     addButton->setMenu(addMenu);
-    addMenu->addAction(_ui->actionAdd_Group);
-    addMenu->addAction(_ui->actionAdd_Model);
+    addMenu->addAction(_ui->actionAdd_Node);
+    addMenu->addAction(_ui->actionAdd_Cube);
+    addMenu->addAction(_ui->actionAdd_Cone);
+    addMenu->addAction(_ui->actionAdd_Cylinder);
+    addMenu->addAction(_ui->actionAdd_Sphere);
     addMenu->addAction(_ui->actionAdd_Camera);
     addMenu->addAction(_ui->actionAdd_DirectionalLight);
     addMenu->addAction(_ui->actionAdd_PointLight);
     addMenu->addAction(_ui->actionAdd_SpotLight);
+    addMenu->addAction(_ui->actionAdd_Terrain);
+    addMenu->addAction(_ui->actionAdd_ParticleEmitter);
+    addMenu->addAction(_ui->actionAdd_Sprite);
+    addMenu->addAction(_ui->actionAdd_Text);
+    addMenu->addAction(_ui->actionAdd_Form);
+    addMenu->addAction(_ui->actionAdd_Audio);
+    addMenu->addAction(_ui->actionAdd_Video);
 
     // Initialize the left toolbar
     toolBar = this->addToolBar(tr("Left"));
